b35e1a0ea2d3f5c40dd90ab385a125b5e6a420cc
adnan5509 <adnanafzal5509@gmail.com>
2025-04-28 21:43:06 +0200

    refactor user tasks component to use route parameters for user name

--- src/app/users/user-tasks/user-tasks.component.ts
+++ src/app/users/user-tasks/user-tasks.component.ts
@@ -1,5 +1,7 @@
-import { Component, computed, input } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { UsersService } from '../users.service';
+import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-user-tasks',
@@ -7,13 +9,29 @@ import { UsersService } from '../users.service';
   templateUrl: './user-tasks.component.html',
   styleUrl: './user-tasks.component.css',
 })
-export class UserTasksComponent {
+export class UserTasksComponent implements OnDestroy {
 
-  constructor(private userService: UsersService) { }
+  constructor(
+    private userService: UsersService,
+    private activatedRoute: ActivatedRoute
+  ) { }
 
-  userId = input.required<string>();
+  userName = '';
+  private routeSub?: Subscription;
 
-  userName = computed(() => this.userService.getUser(this.userId())?.name);
+  ngOnInit() {
+    this.routeSub = this.activatedRoute.paramMap.subscribe({
+      next: (paramMap) => {
+        const id = paramMap.get('userId');  // <<< use the correct param name from your route
+        if (id) {
+          const user = this.userService.getUser(id);
+          this.userName = user?.name || '';
+        }
+      }
+    });
+  }
 
-
+  ngOnDestroy() {
+    this.routeSub?.unsubscribe();
+  }
 }
