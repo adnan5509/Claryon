d9dc9c80f05b2c6620a10f40214eeaba6c5fb412
adnan5509 <adnanafzal5509@gmail.com>
2025-04-29 18:36:38 +0200

    add sorting functionality to user tasks with a sort button

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,24 +1,51 @@
-import { Component, input } from '@angular/core';
+import { Component, input, OnDestroy, OnInit } from '@angular/core';
 
 import { TaskComponent } from './task/task.component';
 import { Task } from './task/task.model';
 import { TasksService } from './tasks.service';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css',
-  imports: [TaskComponent],
+  imports: [TaskComponent, RouterLink],
 })
-export class TasksComponent {
+export class TasksComponent implements OnInit, OnDestroy {
 
-  constructor(private tasksService: TasksService) { }
+  constructor(private tasksService: TasksService, private activatedRoute: ActivatedRoute) { }
 
   userId = input.required<string>();
 
+  subscriptions: Subscription[] = [];
+  order: 'asc' | 'desc' = 'desc';
+
   get userTasks(): Task[] {
-    return this.tasksService.getUserTasks(this.userId());
+    return this.tasksService.getUserTasks(this.userId()).sort((a, b) => {
+      if (this.order === 'asc') {
+        return a.dueDate > b.dueDate ? 1 : -1;
+      } else {
+        return a.dueDate < b.dueDate ? 1 : -1;
+      }
+    });
+  }
+
+  ngOnInit() {
+    this.subscriptions.push(
+      this.activatedRoute.queryParams.subscribe(
+        {
+          next: (param) => {
+            this.order = param['order'];
+          }
+        }
+      )
+    );
+  }
+
+  ngOnDestroy() {
+    this.subscriptions.forEach((sub) => sub.unsubscribe());
   }
 
 }
