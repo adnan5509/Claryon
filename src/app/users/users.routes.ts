d6883828302fe6a16f23516c267005bdb1dd4ae2
adnan5509 <adnanafzal5509@gmail.com>
2025-04-30 14:06:32 +0200

    add AddNewTaskGuard to protect new task route and implement navigation logic

--- src/app/users/users.routes.ts
+++ src/app/users/users.routes.ts
@@ -1,7 +1,17 @@
-import { Routes } from "@angular/router";
+import { CanActivateFn, Router, Routes } from "@angular/router";
 import { NewTaskComponent } from "../tasks/new-task/new-task.component";
 import { TasksComponent, tasksResolver } from "../tasks/tasks.component";
+import { inject } from "@angular/core";
 
+
+const AddNewTaskGaurd: CanActivateFn = (route, segments) => {
+    const router = inject(Router);
+    if (route.params['userId'] === 'u1' || route.params['userId'] === 'u2') {
+        return true;
+    }
+    router.navigate(['/goAway']);
+    return false;
+}
 export const userRoutes: Routes = [
     {
         path: '',
@@ -20,5 +30,6 @@ export const userRoutes: Routes = [
         path: 'tasks/new',
         component: NewTaskComponent,
         title: 'Add New Task',
+        canActivate: [AddNewTaskGaurd]
     }
 ]
